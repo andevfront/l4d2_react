9305e9996dd28fe89f37abb5766a9069feaa16bf
andevfront <ayelen.and@hotmail.com>
2024-07-04 12:51:23 -0300

    diseño de pagina de error 404

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,3 +1,8 @@
 export const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <div className="container mx-auto flex h-screen flex-col items-center justify-center p-10">
+      <h2 className="text-7xl font-bold text-white">404</h2>
+      <p className="text-xl">Page not found</p>
+    </div>
+  );
 };
